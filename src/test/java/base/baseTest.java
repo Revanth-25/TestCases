d881d25e4094f9496af91615de7e0f1c6d0fe3b8
Revanth-25 <142740859+Revanth-25@users.noreply.github.com>
2023-08-24 18:15:35 +0530

    Update baseTest.java

--- src/test/java/base/baseTest.java
+++ src/test/java/base/baseTest.java
@@ -3,28 +3,30 @@ package base;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class baseTest {
-	
+
+	   public static ChromeOptions opt = new ChromeOptions();
 	   public static WebDriver driver;
 	   public static Properties prop = new Properties();
 	   public static FileReader fr;
 	    @BeforeTest
        public void setUp() throws IOException {
-    	  
+
+	                opt.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true); 
     			WebDriverManager.chromedriver().setup();  //base
     			driver = new ChromeDriver(); 
     			driver.get("https://localhost:44340/Orders/Create");
-	                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     	   
        }
 	    @AfterTest
